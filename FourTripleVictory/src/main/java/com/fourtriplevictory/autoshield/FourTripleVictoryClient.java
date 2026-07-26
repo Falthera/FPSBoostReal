aa62d43ac20eb1b9b05df9dcae9b7f735005cd80
Aaryadev <aaryadev@example.com>
2026-07-26 14:19:19 +1000

    Remove all keybindings to test static init hang

--- FourTripleVictory/src/main/java/com/fourtriplevictory/autoshield/FourTripleVictoryClient.java
+++ FourTripleVictory/src/main/java/com/fourtriplevictory/autoshield/FourTripleVictoryClient.java
@@ -1,8 +1,11 @@
 package com.fourtriplevictory.autoshield;
 
 import net.fabricmc.api.ClientModInitializer;
+import net.minecraft.util.Identifier;
 
 public class FourTripleVictoryClient implements ClientModInitializer {
+    public static final Identifier TEST = Identifier.of("fourtriplevictory", "test");
+
     @Override
     public void onInitializeClient() {
     }
